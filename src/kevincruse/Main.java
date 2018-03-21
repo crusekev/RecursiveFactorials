bb1795276042e537d5224620de3d91a05b640218
crusekev <crusekev@gmail.com>
2018-03-21 13:59:38 -0500

    Table of factorials using recursion, where the function to calculate the factorial calls its self. Store the factorials in an array, then print to a text file. Go from 0 to 20 for the factorials

--- src/kevincruse/Main.java
+++ src/kevincruse/Main.java
@@ -4,23 +4,20 @@ import java.io.*;
 
 public class Main {
 
-    public static long fact(long n, long factArray[], int i) {
-        long result;
+    static long fact(int n, long factArray[]) {
         if (n == 0) {
             return 1;
         }
-        result = n * fact(n - 1, factArray, ++i);
-        factArray[i] = result;
-        return result;
+        factArray[n] = n * fact(n - 1, factArray);
+        return n * fact(n - 1, factArray);
     }
 
     public static void main(String[] args) throws IOException {
         PrintWriter outfile = new PrintWriter("data.txt");
         long[] factArray = new long[21];
-        int i = 0;
-        fact(20, factArray, i);
-        for (i = 1; i <= 20; i++) {
-            outfile.println(factArray[i]);
+        fact(20, factArray);
+        for (int n = 1; n <= 20; n++) {
+            outfile.println(factArray[n]);
         }
         outfile.close();
     }
